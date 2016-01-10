7d4d44ddeb47fceedbe16619361a44106a33bca0
yancyn <yancyn@gmail.com>
2016-01-10 14:45:52 +0800

    Add blank Amazon parser test

---
+++ Muje.Parser.Test/AmazonTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Muje.Parser.Amazon;
+using NUnit;
+using NUnit.Framework;
+
+namespace Muje.Parser.Test
+{
+    [TestFixture]
+    public class AmazonTests
+    {
+        [Test]
+        public void Parse()
+        {
+            AmazonParser parser = new AmazonParser("http://www.amazon.com/Best-Sellers-Books-Investing/zgbs/books/2665");
+            parser.Parse();
+
+        }
+    }
+}
