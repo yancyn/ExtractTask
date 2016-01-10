2a1ee2b7fb338ada7a51e9eda9602cae4b0fd448
yancyn <yancyn@gmail.com>
2016-01-10 16:11:07 +0800

    Add AmazonItem and series test

--- Muje.Parser.Test/AmazonTests.cs
+++ Muje.Parser.Test/AmazonTests.cs
@@ -14,11 +14,34 @@ namespace Muje.Parser.Test
     public class AmazonTests
     {
         [Test]
+        public void SeriesTest()
+        {
+            AmazonItem target = new AmazonItem("The Index Card: Why Personal Finance Doesn’t Have to Be Complicated", @"http://www.amazon.com/The-Index-Card-Personal-Complicated/dp/1591847680/ref=zg_bs_2665_1");
+            string expected = "1591847680";
+            string actual = target.Series;
+            Console.WriteLine(actual);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void AmazonItemTest()
+        {
+            AmazonItem target = new AmazonItem("The Index Card: Why Personal Finance Doesn’t Have to Be Complicated", @"http://www.amazon.com/The-Index-Card-Personal-Complicated/dp/1591847680/ref=zg_bs_2665_1");
+            string expected = "The Index Card: Why Personal Finance Doesn’t Have to Be Complicated";
+            string actual = target.Title;
+            Console.WriteLine(actual);
+            Assert.AreEqual(expected, actual);
+
+            expected = @"http://www.amazon.com/The-Index-Card-Personal-Complicated/dp/1591847680/ref=zg_bs_2665_1";
+            actual = target.Url;
+            Console.WriteLine(actual);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
         public void Parse()
         {
-            AmazonParser parser = new AmazonParser("http://www.amazon.com/Best-Sellers-Books-Investing/zgbs/books/2665");
+            AmazonParser parser = new AmazonParser(@"http://www.amazon.com/Best-Sellers-Books-Investing/zgbs/books/2665");
             parser.Parse();
-
+            parser.ListAffiliate();
         }
     }
 }
