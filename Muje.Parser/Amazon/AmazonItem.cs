23452179272f3bd8c521076fef1f610918f12f77
yancyn <yancyn@gmail.com>
2016-01-10 14:41:43 +0800

    Start work on Amazon extract logic

---
+++ Muje.Parser/Amazon/AmazonItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Muje.Parser.Amazon
+{
+    public class AmazonItem
+    {
+        private string title;
+        public string Title { get { return this.title; } }
+
+        private string url;
+        public string Url { get { return this.url; } }
+
+        private string series;
+        public string Series { get { return this.series; } }
+
+        public AmazonItem(string title, string url)
+        {
+            this.title = title;
+            this.url = url;
+        }
+    }
+}
---
+++ Muje.Parser/Amazon/AmazonParser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Muje.Parser.Amazon
+{
+    public class AmazonParser
+    {
+        /// <summary>
+        /// Base Amazon url to start extraction.
+        /// </summary>
+        private string baseUrl;
+
+        private List<AmazonItem> items;
+        /// <summary>
+        /// Returns collection of items found based on base url.
+        /// </summary>
+        public AmazonItem[] Result { get { return this.items.ToArray(); } }
+
+        private Dictionary<string, string> categories;
+        /// <summary>
+        /// Return categories.
+        /// </summary>
+        public Dictionary<string, string> Catagories { get { return this.categories; } }
+
+        public AmazonParser(string url)
+        {
+            this.baseUrl = url;
+            this.categories = new Dictionary<string, string>();
+        }
+        public void Parse()
+        {
+
+        }
+    }
+}
