9bd1cb18683b904345bf1617e03c780f6bf61326
yancyn <yancyn@gmail.com>
2016-01-10 13:41:05 +0800

    Rename ObjectCard to TradeCard
    ObjectCard not a good name

--- Muje.Parser/Trader/TradeCard.cs
+++ Muje.Parser/Trader/TradeCard.cs
@@ -6,7 +6,7 @@ using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace ExtractBusinessCard
+namespace Muje.Parser
 {
     public class TradeCard: CardFactory
     {
