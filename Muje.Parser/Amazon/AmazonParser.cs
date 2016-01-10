61ce7b515d92b4aa3ec001fe2a52593fae9736a7
yancyn <yancyn@gmail.com>
2016-01-10 16:26:42 +0800

    Loop 5 times to extract 100 links from base url

--- Muje.Parser/Amazon/AmazonParser.cs
+++ Muje.Parser/Amazon/AmazonParser.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,9 +30,56 @@ namespace Muje.Parser.Amazon
         public AmazonParser(string url)
         {
             this.baseUrl = url;
+            this.items = new List<AmazonItem>();
             this.categories = new Dictionary<string, string>();
         }
+        /// <summary>
+        /// Extract to collection of AmazonItem.
+        /// </summary>
         public void Parse()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                string link = baseUrl + "#" + (i + 1).ToString();
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(link);
+                System.Diagnostics.Debug.WriteLine("Start parsing " + link);
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+                    {
+                        string line;
+                        bool start = false;
+                        string useful = string.Empty;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            if (line.Contains("<div class=\"zg_itemImageImmersion\">")) start = true;
+                            if (start)
+                            {
+                                useful += line;
+                                if (line.Contains("<img src="))
+                                {
+                                    start = false;
+
+                                    string title = RegexHelper.GrabPattern(useful, "title=\"", "\" onload");
+                                    if (string.IsNullOrEmpty(title)) title = RegexHelper.GrabPattern(useful, "title=\"", "\"/>");
+                                    string url = RegexHelper.GrabPattern(useful, "href=\"", "\"><img");
+                                    AmazonItem item = new AmazonItem(title, url);
+                                    this.items.Add(item);
+                                    System.Diagnostics.Debug.WriteLine(item.ToString());
+
+                                    useful = string.Empty;
+                                }
+                            }
+                        }
+                    }
+                }// close http request
+            }// end loop
+            System.Diagnostics.Debug.WriteLine("Result found: " + items.Count);
+        }
+        /// <summary>
+        /// Generate collection of urls can be use in Amazonaire.
+        /// </summary>
+        public void ListAffiliate()
         {
 
         }
